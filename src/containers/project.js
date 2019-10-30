2ae160cca0f9c4b61533d6229ac67e9fb2031247
dante <40077234+DanteDelgadillo@users.noreply.github.com>
2019-10-30 01:07:24 -0700

    update 1

--- src/containers/project.js
+++ src/containers/project.js
@@ -1,35 +1,45 @@
 import React, { Component } from "react";
 import TodoApp from "../images/todosite.png";
+import ImageManager from "../images/Untitled.png"
 // import Grid from "react-css-grid";
-import { Container, Row, Col } from "react-bootstrap";
+// import { Container, Row, Col } from "react-bootstrap";
 
 class Projects extends Component {
   render() {
     return (
       <React.Fragment>
-        {/* <Grid width={128} gap={16}> */}
-
-        <Container>
-          <Row>
-            <Col >
-              <center>
-                <div className="post">
-
-
-                  <img className="dogImg " src={TodoApp} alt="pic" />
-
-                  <a href="https://stickytodolist.netlify.com" target="_blank" rel="noopener noreferrer" >
-                    <div className="animtion1">
-
-                      <h2 className="font">To do App</h2>
-
-                    </div>
-                  </a>
-                </div>
-              </center>
-            </Col>
-          </Row>
-        </Container>
+        <div className="flex-container">
+          {/* start */}
+          <div className="post">
+            <img className="dogImg " src={TodoApp} alt="pic" />
+            <a href="https://stickytodolist.netlify.com" target="_blank" rel="noopener noreferrer" >
+              <div className="animtion1">
+                <h2 className="font">To do App</h2>
+              </div>
+            </a>
+          </div>
+          {/* end */}
+          {/* start */}
+          <div className="post">
+            <img className="dogImg " src={ImageManager} alt="pic" />
+            <a href="https://imagemanagement.netlify.com" target="_blank" rel="noopener noreferrer" >
+              <div className="animtion1">
+                <h2 className="font">Image Management </h2>
+              </div>
+            </a>
+          </div>
+          {/* end */}
+          {/* start */}
+          {/* <div className="post">
+            <img className="dogImg " src={TodoApp} alt="pic" />
+            <a href="https://stickytodolist.netlify.com" target="_blank" rel="noopener noreferrer" >
+              <div className="animtion1">
+                <h2 className="font">To do App</h2>
+              </div>
+            </a>
+          </div> */}
+          {/* end */}
+        </div>
       </React.Fragment >
     );
   }
